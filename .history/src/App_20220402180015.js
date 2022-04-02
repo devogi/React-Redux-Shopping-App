5aef8a3e6b717d66330d0bf5da00690cbabfcf8a
devogi <osalkim12@gmail.com>
2022-04-02 18:08:17 +0300

    third

---
+++ .history/src/App_20220402180015.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { BrowserRouter as Router, Ro Route } from "react-router-dom";
+import "./App.css";
+import Header from "./containers/Header";
+import ProductDetail from "./containers/ProductDetail";
+import ProductListing from "./containers/ProductListing";
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Header />
+        <Routes>
+          <Route path="/" element={<ProductListing />} />
+          <Route path="/product/:productId " element={<ProductDetail />} />
+          <Route>404 Not Found!</Route>
+        </Routes>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
